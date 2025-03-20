57fa9a497938c37416e9254251de4fe28e22f796
Yixiang Gao <yg5d6@mail.missouri.edu>
2025-03-20 21:20:22 +0000

    add map loading

--- src/localization.cpp
+++ src/localization.cpp
@@ -11,11 +11,26 @@ class LocalizationPublisher : public rclcpp::Node
         LocalizationPublisher()
         : Node("pcd_localization")
         {
+            // Subcribe to LiDAR inputs
             lidarsub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
                 "/spot/lidar/points", 10, 
                 std::bind(&LocalizationPublisher::topic_callback, this, std::placeholders::_1));
 
-            RCLCPP_INFO(this->get_logger(), "Localization node initialized. Subscribed to /spot/lidar/points");
+            RCLCPP_INFO(this->get_logger(), "Subscribed to /spot/lidar/points");
+
+            // Load map file
+            pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+            const std::string map_path = "/root/ros2_ws/src/spot_ros2_gazebo/spot_navigation/maps/simple_tunnel.pcd";
+            if (pcl::io::loadPCDFile<pcl::PointXYZ>(map_path, *map_cloud) == -1)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Failed to load map file: %s", map_path.c_str());
+                return;
+            }
+
+            RCLCPP_INFO(this->get_logger(), "Successfully loaded map with %zu points", map_cloud->size());
+            
+            RCLCPP_INFO(this->get_logger(), "Localization node initialized.");
+
         }
 
     private:
@@ -25,9 +40,6 @@ class LocalizationPublisher : public rclcpp::Node
             RCLCPP_INFO(this->get_logger(), "Received point cloud:");
             RCLCPP_INFO(this->get_logger(), "  Width: %u", msg->width);
             RCLCPP_INFO(this->get_logger(), "  Height: %u", msg->height);
-            RCLCPP_INFO(this->get_logger(), "  Point step: %u", msg.point_step);
-            RCLCPP_INFO(this->get_logger(), "  Row step: %u", msg.row_step);
-            RCLCPP_INFO(this->get_logger(), "  Total points: %u", msg.width * msg.height);
         }
         rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidarsub_;
 };
